5094f24138797724f7ebf7f782f848f688c7b9b2
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-02-06 10:11:19 +0100

    chore: bump version

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-module.exports = require("./base.js");
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: "lf",
+  trailingComma: "none",
+  semi: true,
+  bracketSpacing: true,
+  singleQuote: false,
+  quoteProps: "consistent",
+  bracketSameLine: true,
+  plugins: [require("@ianvs/prettier-plugin-sort-imports")],
+  importOrder: ["^(@|~)(.*)/(.*)$", "^[./]"],
+  importOrderBuiltinModulesToTop: true,
+  importOrderCaseInsensitive: false,
+  importOrderParserPlugins: ["typescript", "jsx", "classProperties"],
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: false,
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true
+};
--- tailwind.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  ...require("./base.js"),
-  plugins: [require("@ianvs/prettier-plugin-sort-imports"), require("prettier-plugin-tailwindcss")]
-};
